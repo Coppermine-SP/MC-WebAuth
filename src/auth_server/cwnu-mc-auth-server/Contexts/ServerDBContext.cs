a5e06373dc0d370b53a83b15c3fd3378adde3961
Coppermine-SP <son5121502@gmail.com>
2024-06-26 04:43:25 +0900

    init: Spigot plugin project init

---
+++ src/auth_server/cwnu-mc-auth-server/Contexts/ServerDBContext.cs
@@ -0,0 +1,15 @@
+﻿using cwnu_mc_auth_server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace cwnu_mc_auth_server.Contexts
+{
+    public class ServerDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Dept> Depts { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data source=");
+    }
+}
---
+++ src/plugin/WebAuth/src/main/java/corp/cloudint/webauth/WebAuth.java
@@ -0,0 +1,17 @@
+package corp.cloudint.webauth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WebAuth extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
