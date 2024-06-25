3a20e29bd8addfbf3f7c3b0b488c028e09224f6e
Coppermine-SP <son5121502@gmail.com>
2024-06-26 04:35:27 +0900

    add: DB 모델

---
+++ src/auth_server/cwnu-mc-auth-server/Models/Dept.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace cwnu_mc_auth_server.Models
+{
+    public class Dept
+    {
+        [Key]
+        public int DeptId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ src/auth_server/cwnu-mc-auth-server/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace cwnu_mc_auth_server.Models
+{
+    public class User
+    {
+        [Key]
+        public string Uuid { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Dept))]
+        public int DeptId { get; set; }
+    }
+}
