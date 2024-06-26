4d1b629fd949ac61a5e7aea291e6773cbd3985c5
Coppermine-SP <son5121502@gmail.com>
2024-06-26 21:10:29 +0900

    add: Util class

---
+++ src/auth_server/cwnu-mc-auth-server/Services/Util.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace cwnu_mc_auth_server.Services
+{
+    public static class Util
+    {
+        public static string GetSHA256Hash(string x)
+        {
+            var sb = new StringBuilder();
+            using (var hash = SHA256.Create())
+            {
+                var result = hash.ComputeHash(Encoding.UTF8.GetBytes(x));
+                for (int i = 0; i < result.Length; i++)
+                    sb.Append(result[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
