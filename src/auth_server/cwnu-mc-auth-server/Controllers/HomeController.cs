7488311e8ff190a0c1e9381a7bc94ea041cf8fdd
Coppermine-SP <son5121502@gmail.com>
2024-06-26 21:26:08 +0900

    feat: User Verfication

--- src/auth_server/cwnu-mc-auth-server/Controllers/HomeController.cs
+++ src/auth_server/cwnu-mc-auth-server/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using cwnu_mc_auth_server.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using cwnu_mc_auth_server.Contexts;
 using cwnu_mc_auth_server.Services;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text;
 
 namespace cwnu_mc_auth_server.Controllers
 {
@@ -9,11 +12,13 @@ namespace cwnu_mc_auth_server.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IVerificationService _verificationService;
+        private readonly ServerDBContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger, IVerificationService verificationService)
+        public HomeController(ILogger<HomeController> logger, IVerificationService verificationService, ServerDBContext context)
         {
             _logger = logger;
             _verificationService = verificationService;
+            _dbContext = context;
         }
 
         public IActionResult Index()
@@ -24,6 +29,7 @@ namespace cwnu_mc_auth_server.Controllers
 
         [HttpPost]
         [AutoValidateAntiforgeryToken]
+        [Route("Verification")]
         public IActionResult Verification(string authcode)
         {
             VerificationRequestModel? model;
@@ -43,34 +49,55 @@ namespace cwnu_mc_auth_server.Controllers
             ViewBag.uuid = model.PlayerUuid;
             ViewBag.nickName = model.PlayerName;
 
-            return View();
+            var viewModel = new VerificationViewModel()
+            {
+                Depts = _dbContext.Depts.ToList()
+            };
+            return View(viewModel);
         }
 
+        [Route("CompleteVerification")]
         public IActionResult CompleteVerification(string token)
         {
             if (String.IsNullOrWhiteSpace(token)) return BadRequest();
 
-            if (_verificationService.CompleteVerification(token)) return View("VerificationCompleted");
+            if (_verificationService.CompleteVerification(_dbContext, token)) return View("VerificationCompleted");
             else return View("VerificationFailed");
         }
 
         [HttpPost]
         [AutoValidateAntiforgeryToken]
+        [Route("RequestVerificationToken")]
         public IActionResult RequestVerificationToken(string authcode, int dept, int studentId)
         {
             VerificationRequestModel model;
             if (!_verificationService.CheckVerificationRequest(authcode, out model) || model is null)
             {
                 TempData["errMessage"] = "인증 코드가 만료되었거나 유효하지 않습니다.";
-                return Redirect("Index");
+                return Redirect("/");
             }
 
             if (model.VerificationToken is not null)
             {
                 TempData["errMessage"] = "이미 인증 메일을 발송했습니다.";
-                return Redirect("Index");
+                return Redirect("/");
             }
 
+            var usr = _dbContext.Users.SingleOrDefault(x => x.StudentId == Util.GetSHA256Hash(studentId.ToString()));
+            var dpt = _dbContext.Depts.SingleOrDefault(x => x.DeptId == dept);
+
+            if (usr is not null)
+            {
+                TempData["errMessage"] = $"이미 이 학번으로 재학생 인증을 받았습니다.";
+                return Redirect("/");
+            }
+
+            if (dpt is null)
+            {
+                TempData["errMessage"] = $"올바르지 않은 학과 데이터";
+                return Redirect("/");
+            }
+                
             model.SetVerificationToken();
             model.DeptCode = dept;
             model.StudentId = studentId.ToString();
--- src/plugin/WebAuth/src/main/java/corp/cloudint/webauth/WebAuth.java
+++ src/plugin/WebAuth/src/main/java/corp/cloudint/webauth/WebAuth.java
@@ -1,12 +1,13 @@
 package corp.cloudint.webauth;
-
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.HashMap;
 
 public final class WebAuth extends JavaPlugin {
-
+    private var deptMap = new HashMap<String, String>();
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
 
     }
 
