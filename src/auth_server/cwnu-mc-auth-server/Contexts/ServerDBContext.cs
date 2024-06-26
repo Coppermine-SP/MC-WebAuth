b13d2ffb2e29c6245121e94d4b2c56981fce5497
Coppermine-SP <son5121502@gmail.com>
2024-06-26 21:04:50 +0900

    chore: 공백 제거

--- src/auth_server/cwnu-mc-auth-server/Contexts/ServerDBContext.cs
+++ src/auth_server/cwnu-mc-auth-server/Contexts/ServerDBContext.cs
@@ -8,11 +8,8 @@ namespace cwnu_mc_auth_server.Contexts
     {
         private IConfiguration _config;
         public ServerDBContext(IConfiguration config) => _config = config;
-        
-
         public DbSet<User> Users { get; set; }
         public DbSet<Dept> Depts { get; set; }
-
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseMySQL(_config.GetConnectionString("ServerDBContext"));
